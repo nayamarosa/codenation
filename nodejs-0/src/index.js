d6c10d1bb4c39ed5a6184687070016bb5699531a
NayamaRosa <falecom@nayama.com.br>
2020-05-07 14:04:22 -0300

    testes 100%

--- nodejs-0/src/index.js
+++ nodejs-0/src/index.js
@@ -4,14 +4,13 @@ const fibonacci = () => {
     let fibonacciArray = [0, 1];
     let maxNumber = 350;
     
-    for(i = 2; i >= 0; i++) {
+    for(let i = 2; i >= 0; i++) {
         
         let fibonacciCalc = fibonacciArray[i-1] + fibonacciArray[i-2];
         
         fibonacciArray.push(fibonacciCalc);
         
         if (fibonacciCalc >= maxNumber) {
-            fibonacciArray.pop();
             break;
         }
     }
