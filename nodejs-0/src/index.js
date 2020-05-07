654892ad0821fb03a2a9ab36f9d3c865ae09f736
NayamaRosa <falecom@nayama.com.br>
2020-05-07 13:58:39 -0300

    const fibonacci

--- nodejs-0/src/index.js
+++ nodejs-0/src/index.js
@@ -1,6 +1,12 @@
 'use strict'
 
-const fibonacci = () => null
+const fibonacci = () => {
+    let fibonacciNum = [0, 1];
+
+    for(i = 0; i <= 350; i++) {
+        return fibonacciNum[i-1] + fibonacciNum[i-2];
+    }
+}
 
 const isFibonnaci = (num) => null
 
