13b111f25ce2ca868dbeab9817c3dd26967e68eb
NayamaRosa <falecom@nayama.com.br>
2020-05-28 09:47:24 -0300

    começando de novo - desafio 4 e 5 - base

--- react-13/src/components/Contact.jsx
+++ react-13/src/components/Contact.jsx
@@ -2,23 +2,8 @@ import React from 'react';
 
 class Contact extends React.Component {
   render() {
-    return (
-      <div className="container">
-        <section className="contacts">
-          <article className="contact">
-            <img className="contact__avatar" src={this.props.data.avatar} alt={this.props.data.name} />
-            <span className="contact__data">{this.props.data.name}</span>
-            <span className="contact__data">{this.props.data.phone}</span>
-            <span className="contact__data">{this.props.data.country}</span>
-            <span className="contact__data">{this.props.data.admissionDate}</span>
-            <span className="contact__data">{this.props.data.company}</span>
-            <span className="contact__data">{this.props.data.department}</span>
-          </article>
-        </section>
-      </div>
-      );
-    }
+    return (<div />);
   }
-  
-  export default Contact;
-  
+}
+
+export default Contact;
--- react-13/src/components/Contacts.jsx
+++ react-13/src/components/Contacts.jsx
@@ -1,28 +1,9 @@
 import React from "react";
 
-import Contact from '../components/Contact'
-
 class Contacts extends React.Component {
 	render() {
-    const { contacts } = this.props;
-		return (
-			<div className="container">
-        <section className="contacts">
-          <article className="contact">
-            <span className="contact__avatar" />
-            <span className="contact__data">Nome</span>
-            <span className="contact__data">Telefone</span>
-            <span className="contact__data">País</span>
-            <span className="contact__data">Admissão</span>
-            <span className="contact__data">Empresa</span>
-            <span className="contact__data">Departamento</span>
-          </article>
-        {contacts.map((person) => <Contact data={person} key={person.id} />)}
-        </section>
-			</div>
-			);
-		}
+		return (<div />);
 	}
-	
-	export default Contacts;
-	
+}
+
+export default Contacts;
