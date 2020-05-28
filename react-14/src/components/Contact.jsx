c64a038ef554c83881880cfed7197f0f9d26ebd0
NayamaRosa <falecom@nayama.com.br>
2020-05-28 10:40:00 -0300

    desafio 5 passando testes, desafio 6 trazendo lista na tela

--- react-14/src/components/Contact.jsx
+++ react-14/src/components/Contact.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import './Contacts.scss'
+
 class Contact extends React.Component {
   render() {
-    return (<div />);
+    return (
+      <article className="contact" data-testid="contact">
+        <img className="contact__avatar" src={this.props.data.avatar} alt={this.props.data.name} />
+        <span className="contact__data">{this.props.data.name}</span>
+        <span className="contact__data">{this.props.data.phone}</span>
+        <span className="contact__data">{this.props.data.country}</span>
+        <span className="contact__data">{this.props.data.admissionDate}</span>
+        <span className="contact__data">{this.props.data.company}</span>
+        <span className="contact__data">{this.props.data.department}</span>
+      </article>
+      );
+    }
   }
-}
-
-export default Contact;
+  
+  export default Contact;
+  
