b369bc0d0b958de8ab83b4e4a3f9d9986ec87383
NayamaRosa <falecom@nayama.com.br>
2020-06-09 21:27:02 -0300

    userform, usersposts, usersprofile e userslist

--- react-16/src/containers/UserForm/UserForm.jsx
+++ react-16/src/containers/UserForm/UserForm.jsx
@@ -1,81 +1,130 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// import SuccessMessage from '../../components/SuccessMessage';
+import SuccessMessage from '../../components/SuccessMessage';
 
-// import './UserForm.scss';
+import './UserForm.scss';
 
-// const UserForm = () => {
-//   return (
-//     <React.Fragment>
-//       <section className="profile" data-testid="user-form">
-//         <div className="container">
-//           <div className="profile-data">
-//             <div className="user">
-//               <div className="user__thumb">
-//                 {avatar
-//                   ? <img src={avatar} alt="" />
-//                   : <img src="https://viniciusvinna.netlify.app/assets/api-instagram/profiles/profile-placeholder.png" alt="" />
-//                 }
-//               </div>
+const UserForm = () => {
+  const [name, setName] = useState('John Doe');
+  const [avatar, setAvatar] = useState('');
+  const [username, setUsername] = useState('johndoe');
+  const [email, setEmail] = useState('johndoe@gmail.com');
+  const [submit, setSubmit] = useState(false);
 
-//               {name && (
-//                 <p className="user__name">
-//                   {name}
-//                   <span>@{username}</span>
-//                 </p>
-//               )}
-//             </div>
-//           </div>
-//         </div>
-//       </section>
+  const handleSetName = (event) => {
+    const { value } = event.target;
 
-//       <section className="post__form">
-//         <div className="container">
-//           <div className="post__form__wrapper">
-//             <label>Nome</label>
-//             <input
-//               type="text"
-//               value={name}
-//               placeholder="Ex: Fulano da Silva"
-//               onChange={(event) => handleSetName(event)}
-//             />
+    setName(value);
+  };
 
-//             <label>Usuário</label>
-//             <input
-//               type="text"
-//               value={username}
-//               placeholder="Ex: fulano_da_silva"
-//               onChange={(event) => handleSetUserName(event)}
-//             />
+  const handleSetAvatar = (event) => {
+    const { value } = event.target;
 
-//             <label>Email</label>
-//             <input
-//               type="email"
-//               value={email}
-//               placeholder="Ex: fulano@provedor.com"
-//               onChange={(event) => handleSetEmail(event)}
-//             />
+    setAvatar(value);
+  };
 
-//             <label>Url da Imagem de Perfil (use a url da imagem do Linkedin)</label>
-//             <input
-//               type="text"
-//               placeholder="http://..."
-//               onChange={(event) => handleSetAvatar(event)}
-//             />
+  const handleSetUserName = (event) => {
+    const { value } = event.target;
 
-//             <button
-//               type="button"
-//               onClick={(event) => handleAddUser(event)}
-//             >
-//               Cadastrar
-//             </button>
-//           </div>
-//         </div>
-//       </section>
+    setUsername(value);
+  };
 
-//       {submit && (<SuccessMessage />)}
-//     </React.Fragment>
-//   );
-// };
+  const handleSetEmail = (event) => {
+    const { value } = event.target;
 
-// export default UserForm;
+    setEmail(value);
+  };
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+
+    const postObject = JSON.stringify({
+      name,
+      avatar,
+      username,
+      email,
+    });
+
+    fetch('https://5e7d0266a917d70016684219.mockapi.io/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: postObject
+    }).then(() => setSubmit(true));
+  };
+
+  return (
+    <React.Fragment>
+      <section className="profile" data-testid="user-form">
+        <div className="container">
+          <div className="profile-data">
+            <div className="user">
+              <div className="user__thumb">
+                {avatar
+                  ? <img src={avatar} alt="" />
+                  : <img src="https://viniciusvinna.netlify.app/assets/api-instagram/profiles/profile-placeholder.png" alt="" />
+                }
+              </div>
+
+              {name && (
+                <p className="user__name">
+                  {name}
+                  <span>@{username}</span>
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="post__form">
+        <div className="container">
+          <div className="post__form__wrapper">
+            <label>Nome</label>
+            <input
+              type="text"
+              value={name}
+              placeholder="Ex: Fulano da Silva"
+              onChange={(event) => handleSetName(event)}
+            />
+
+            <label>Usuário</label>
+            <input
+              type="text"
+              value={username}
+              placeholder="Ex: fulano_da_silva"
+              onChange={(event) => handleSetUserName(event)}
+            />
+
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              placeholder="Ex: fulano@provedor.com"
+              onChange={(event) => handleSetEmail(event)}
+            />
+
+            <label>Url da Imagem de Perfil (use a url da imagem do Linkedin)</label>
+            <input
+              type="text"
+              placeholder="http://..."
+              onChange={(event) => handleSetAvatar(event)}
+            />
+
+            <button
+              type="button"
+              onClick={(event) => handleAddUser(event)}
+            >
+              Cadastrar
+            </button>
+          </div>
+        </div>
+      </section>
+
+      {submit && (<SuccessMessage />)}
+    </React.Fragment>
+  );
+};
+
+export default UserForm;
