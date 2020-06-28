29c2591fb28afcc0719885c2b6fcc3f7af5615e1
NayamaRosa <falecom@nayama.com.br>
2020-06-28 20:13:20 -0300

    fazendo estrutura de pastas e arquivos

--- react-17/src/config/index.js
+++ react-17/src/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   spotify: {
     authorizationURL: 'https://accounts.spotify.com/authorize',
-    clientId: '',
+    clientId: 'e14aa7650196476496859e5ab583ca64',
     redirectUrl: `${window.location.origin}/authorize`,
     webAPI: 'https://api.spotify.com/v1',
     scopes: [
